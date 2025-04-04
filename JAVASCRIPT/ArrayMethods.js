bfd396333986b4355ec2d50b18f6aa8d4030f612
vinodkurra <ugandharkurra1996@gmail.com>
2025-04-04 08:19:49 +0530

    Add examples of array methods in JavaScript

--- JAVASCRIPT/ArrayMethods.js
+++ JAVASCRIPT/ArrayMethods.js
@@ -1,3 +1,73 @@
 const names = ["vinod", 1, { name: "Sam", age: 28 }, [1, 2, 3, 4]];
 
 // const arrayName = [item1, ietm2, ... ]  //
+
+const names2 = ["apple", "orange", "grape", "one", "two", "three"];
+
+const names_1 = new Array("one", "two", "three");
+
+// names.length = 2
+
+// console.log( names, names_1);
+
+// const length = names.length;
+
+// console.log(length)
+
+// const first = names[0];
+
+// const last = names[names.length-1]
+
+// names[2] = "five"
+
+// console.log( names,first , last);
+
+// const arrayToString =  names.toString()
+
+// const stringToArray =  arrayToString.split(",")
+
+// console.log( names, arrayToString, stringToArray);
+
+// const data = {name  : "vinod"};
+// const data1 = [1, 2, 3,]
+
+// console.log(Array.isArray(data1));
+
+string; // 10%
+numbers; // 5%
+arrays; // 65%
+objects; // 20%
+
+const names34 = ["apple", "orange", "grape", "mango"];
+const cars = ["benz", "ferrari", "honda"];
+const numbers = [1, 2, 3, 4, 5];
+
+const string = names.join("/");
+
+// names.pop()
+// names.shift()
+
+// names.unshift("lemon")
+// names.push("show more")
+
+// const newValue = names.concat(cars, numbers);
+
+// const newValue = names.concat("new value");
+
+//   names.copyWithin(2, 0, 2)
+
+// const newArrays = [names, cars, numbers];
+
+// const newArrayData = newArrays.flat()
+
+//  names.splice(2, 2, "Lemon", "Kiwi" )
+
+// 1 position where new elements should be added // 2
+
+// how many elements to be removed from that position // 1
+
+// rest parameters define the new elements to be added  "one", "two"
+
+names.splice(1, 2);
+
+console.log(names);
